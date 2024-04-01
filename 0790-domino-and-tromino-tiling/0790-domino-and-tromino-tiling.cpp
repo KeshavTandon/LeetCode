cc70a8cca90d9fe6a3bfdbcddffa18f81287c801
KeshavTandon <113711996+KeshavTandon@users.noreply.github.com>
2024-04-01 14:51:00 +0530

    Time: 4 ms (14.58%), Space: 7.5 MB (50.79%) - LeetHub

--- 0790-domino-and-tromino-tiling/0790-domino-and-tromino-tiling.cpp
+++ 0790-domino-and-tromino-tiling/0790-domino-and-tromino-tiling.cpp
@@ -1,14 +1,15 @@
 class Solution {
 public:
-    int dp[100001];
-    int MOD=1e9+7;
     int numTilings(int n) {
-       memset(dp,-1,sizeof(dp)); 
-       dp[0]=0,dp[1]=1,dp[2]=2,dp[3]=5;
-       for(int i=4;i<=n;i++)
-       {
-           dp[i]=((2*dp[i-1])%MOD+(dp[i-3])%MOD)%MOD;
-       }
+        int MOD=1e9+7;
+        vector<int>dp(n+1);
+        dp[0]=0,dp[1]=1;
+        if(n>=2)dp[2]=2;
+        if(n>=3)dp[3]=5;
+        for(int i=4;i<=n;i++)
+        {
+            dp[i]=(2*dp[i-1]%MOD+dp[i-3]%MOD)%MOD;
+        }
         return dp[n];
     }
 };
