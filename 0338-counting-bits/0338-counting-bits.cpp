d8e124e80df18741a80cc3648a42ca983ae7a46d
KeshavTandon <113711996+KeshavTandon@users.noreply.github.com>
2024-08-07 22:15:35 +0530

    Time: 0 ms (100.00%), Space: 9.3 MB (58.28%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -1,26 +1,37 @@
 class Solution {
 public:
-    void solve(vector<int>&dp,int n,int index)
-    {
-        if(index==n){
-            if(index%2==0)
-            {
-                dp[index]=dp[index/2];
-            }else
-                dp[index]=dp[index/2]+1;
-            return;
-        }
-        if(index%2==0)
-            dp[index]=dp[index/2];
-        if(index%2!=0)
-            dp[index]=dp[index/2]+1;
-        solve(dp,n,index+1);
-    }
     vector<int> countBits(int n) {
-        vector<int>dp(n+1,-1);
+        vector<int>dp(n+1);
         dp[0]=0;
         if(n==0) return dp;
-        else solve(dp,n,1);
+        dp[1]=1;
+        if(n==1) return dp;
+        dp[2]=1;
+        if(n==2) return dp;
+        for(int i=3;i<=n;i++)
+        {
+            if(i&1)
+            {
+                dp[i]=dp[i/2]+1;
+            }
+            else
+            {
+                dp[i]=dp[i/2];
+            }
+        }
         return dp;
     }
 };
+  // 0->0
+  // 1->1
+  // 2->1
+  // 3->2
+  // 4->1
+  // 5->2
+  // 6->2
+  // 7->3
+  // 8->1
+  // 9->2
+  // 10->2
+  // =>even -> /2
+  // =>odd  -> /2+1
